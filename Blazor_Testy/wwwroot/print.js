03d1cf413b5b407a1824ad09bea876af75dd74d6
Saeiron <saeiron.dev@gmail.com>
2024-09-29 15:40:29 +0200

    More typos

---
+++ Blazor_Testy/wwwroot/print.js
@@ -0,0 +1,14 @@
+﻿// print.js
+function printComponent(componentSelector) {
+    var elementToPrint = document.querySelector(componentSelector);
+    if (elementToPrint) {
+        var printWindow = window.open('', '_blank');
+        printWindow.document.open();
+        printWindow.document.write('<html><head><title>Print</title></head><body>');
+        printWindow.document.write(elementToPrint.innerHTML);
+        printWindow.document.write('</body></html>');
+        printWindow.document.close();
+        printWindow.print();
+        printWindow.close();
+    }
+}
